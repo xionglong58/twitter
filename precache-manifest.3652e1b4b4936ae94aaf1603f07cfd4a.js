3b36a71349272fabb5cfdc6819bcdb2f21d2d62a
xionglong58 <xionglong58@email.swu.edu.cn>
2019-07-23 21:05:18 +0800

    Updates

---
+++ precache-manifest.3652e1b4b4936ae94aaf1603f07cfd4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f1ab8f145f91c39e805c01201a45e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e062cb538c20be6ce7b",
+    "url": "./static/css/2.d19c8617.chunk.css"
+  },
+  {
+    "revision": "7e062cb538c20be6ce7b",
+    "url": "./static/js/2.8e76ab7e.chunk.js"
+  },
+  {
+    "revision": "7c7e386a951175f8b71f",
+    "url": "./static/js/main.ec606157.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
